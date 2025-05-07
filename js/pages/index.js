1f9f21cb093e463471fc44818e30ccf91ac8a934
Pedro Augusto <pedro.augusto120105@gmail.com>
2025-05-06 22:38:10 -0300

    feat: adicionado a tela de verificação

---
+++ js/pages/index.js
@@ -0,0 +1,42 @@
+$("#form").validate(
+    {
+        rules:{
+            login:{
+                required: true
+            },
+            senha:{
+                required: true
+            }
+        },
+        messages:{
+            login:{
+                required: "Campo Obrigatório"
+            },
+            senha:{
+                required: "Campo Obrigatório"
+            }
+        }
+    }
+);
+
+async function autenticar() {
+    if($("#form").valid()) {
+        let login = $("#login").val();
+        let senha = $("#senha").val();
+
+        try {
+            let resposta = await fetch(`https://api-odinline.odiloncorrea.com/usuario/${login}/${senha}/autenticar`);
+            let usuario = await resposta.json();
+
+            if(usuario.id > 0) {
+                localStorage.setItem('usuarioAutenticado', JSON.stringify(usuario));
+                window.location.href = "menuPage.html";
+            } else {
+                alert("Usuário ou senha inválidos.")
+            }
+        } catch (error) {
+            alert("Erro ao tentar autenticar.")
+        }
+    }
+}
+
